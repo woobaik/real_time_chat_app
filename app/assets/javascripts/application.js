32a3779d81c6387cdb139dbfaaa6005b9c3cfcec
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-02-16 16:07:37 -0800

    Bug fix for dropdown error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,12 +19,7 @@
 
 $(document).ready(function() {
   $('.ui.dropdown').dropdown();
-  $('.message .close')
-  .on('click', function() {
-    $(this)
-      .closest('.message')
-      .transition('fade')
-    ;
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
   })
-;
 })
