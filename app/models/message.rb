d64869edbb6810e681c035630f499267b4e96ae7
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-02-20 02:32:28 -0800

    Create scope for custom message display

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   validates :body, presence: true, length: { minimum: 1, maximum: 255}
   validates_presence_of :user
+
+  scope :custom_display, -> { order(:created_at).last(17)}
 end
